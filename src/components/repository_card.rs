f1bfccc4fd30a18e3144fa42cfce60e86547a4a9
Nic McPhee <mcphee@morris.umn.edu>
2022-08-30 10:44:07 -0500

    Add checkboxes to `RepositoryCard`
    This turns the `RepositoryCard` component into a DaisyUI card and adds a checkbox element at the top of every card.

--- src/components/repository_card.rs
+++ src/components/repository_card.rs
@@ -19,20 +19,34 @@ pub struct Props {
 pub fn repository_card(props: &Props) -> Html {
     let Props { repository } = props;
     html! {
-        <div>
-            if repository.archived {
-                <h2 class="text-2xl text-gray-300">{ &repository.name }</h2>
-            } else {
-                <h2 class="text-2xl">{ &repository.name }</h2>
-            }
-            {
-                repository.description.as_ref().map_or_else(
-                    || html! { <p class="text-blue-700">{ "There was no description for this repository "}</p> },
-                    |s| html! { <p class="text-green-700">{ s }</p> }
-                )
-            }
-            <p>{ format!("Last updated on {}", repository.updated_at.format("%Y-%m-%d")) }</p>
-            <p>{ format!("Last pushed to on {}", repository.pushed_at.format("%Y-%m-%d")) }</p>
+        <div class="card card-compact">
+            <div class="card-body">
+                if repository.archived {
+                    <p class="italic">{ "This repository is already archived" }</p>
+                } else {
+                    <div class="card-actions">
+                        <div class="form-control">
+                            <label class="label cursor-pointer">
+                                <input type="checkbox" checked=true class="checkbox" />
+                                <p class="label-text italic ml-2">{ "Archive this repository" }</p> 
+                            </label>
+                        </div>
+                    </div>
+                }
+                if repository.archived {
+                    <h2 class="card-title text-gray-300">{ &repository.name }</h2>
+                } else {
+                    <h2 class="card-title">{ &repository.name }</h2>
+                }
+                {
+                    repository.description.as_ref().map_or_else(
+                        || html! { <p class="text-blue-700">{ "There was no description for this repository "}</p> },
+                        |s| html! { <p class="text-green-700">{ s }</p> }
+                    )
+                }
+                <p>{ format!("Last updated on {}; ", repository.updated_at.format("%Y-%m-%d")) }
+                   { format!("last pushed to on {}", repository.pushed_at.format("%Y-%m-%d")) }</p>
+            </div>
         </div>
     }
 }
