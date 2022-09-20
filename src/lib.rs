48bf5c0b3294d2fa0dd20db92ea19ff9f5a1b9e0
Nic McPhee <mcphee@morris.umn.edu>
2022-09-20 12:14:16 -0500

    Move the `Route` enum to `lib.rs`
    I needed access to the `Route` enum in order to change "pages" from the paginator to the review-and-submit component, so I moved it from `main.rs` to `lib.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
+use yew_router::Routable;
+
 pub mod components;
 pub mod repository;
+
+#[derive(Clone, Routable, PartialEq)]
+pub enum Route {
+    #[at("/ice-repos/review-and-submit")]
+    ReviewAndSubmit,
+    #[at("/ice-repos/about")]
+    About,
+    #[not_found]
+    #[at("/ice-repos/404")]
+    NotFound,
+}
